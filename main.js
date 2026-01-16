90ad79d4e35f124a0ae881ee1e731bc95343e490
Jyoti Patel <jp2746360@gmail.com>
2026-01-16 20:13:57 +0530

    Update typed strings for typing effect

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var typed = new Typed("#typing", {
-    strings: ["Backend Developer", "Python Developer"],
+    strings: ["Software Engineer", "Cloud Engineer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
+
